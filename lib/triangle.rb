2b2b5599b197bb4000c6b2435786b12c98fb8a9e
Daniel Dawson <dannyldawson@gmail.com>
2018-12-19 18:39:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,6 @@ class Triangle
   attr_accessor :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
-    if side_one <= 0 || side_two <= 0 || side_three <= 0 
-      begin
-        raise TriangleError
-      rescue TriangleError => error 
-        puts error.message
-      end
-    else
       @side_one = side_one
       @side_two = side_two
       @side_three = side_three
