1cf2cd92a261f3026ae8481d3f9f7f3201b62ff8
Daniel Dawson <dannyldawson@gmail.com>
2018-12-19 18:37:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,6 @@ class Triangle
   attr_accessor :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
     if side_one <= 0 || side_two <= 0 || side_three <= 0 
       begin
         raise TriangleError
@@ -13,6 +10,9 @@ class Triangle
         puts error.message
       end
     end
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end
   
   def kind 
