5580ce7abccdfdf80483a5fa2eae3d01d9f0386d
Daniel Dawson <dannyldawson@gmail.com>
2018-12-19 18:34:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,9 @@ class Triangle
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
+    if side_one <= 0 || side_two <= 0 || side_three <= 0 
+      begin
+      raise 
   end
   
   def kind 
