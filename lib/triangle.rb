6d12f257e3780c9cb84d3c4b15dc6f866988942f
Daniel Dawson <dannyldawson@gmail.com>
2018-12-19 18:39:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,11 +6,16 @@ class Triangle
       @side_one = side_one
       @side_two = side_two
       @side_three = side_three
-    end
   end
   
   def kind 
-    if side_one == side_two && side_two == side_three
+    if side_one <= 0 || side_two <= 0 || side_three <= 0 
+      begin
+        raise TriangleError
+      rescue TriangleError => error 
+        puts error.message
+      end
+    elsif side_one == side_two && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_two == side_three || side_one == side_three
       :isosceles
