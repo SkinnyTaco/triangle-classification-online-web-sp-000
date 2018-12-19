e07190edd1aa728f6c875dfcc2ae5866ab5cf79f
Daniel Dawson <dannyldawson@gmail.com>
2018-12-19 18:45:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
   end
   
   def kind 
-    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
-      || @side_one + @side_two <= @side_three
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two <= @side_three
       begin
         raise TriangleError
       end
